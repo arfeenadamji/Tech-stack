e55a94ce9d01ddc7d6975f25f80229025e341cd5
arfeenadamji <arfeenm9@gmail.com>
2020-10-26 01:49:35 +0500

    first commit

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import libraryReducers from './libraryReducers';
-import SelectionReducer from './SelectionReducer';
-
-export default combineReducers({
-    libraries: libraryReducers,
-    selectedLibraryId: SelectionReducer
-});
